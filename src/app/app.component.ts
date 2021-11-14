7d8eb2b8b6813565467977afe109ec369268c2e5
Jana Semenova <jana.semenova@yolawo.de>
2021-11-14 07:27:57 +0100

    Angular Custom Services - The Injectable Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
 import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
   selector: "app-root",
@@ -23,7 +24,7 @@ export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
   //courses;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private coursesService: CoursesService) {}
 
   ngOnInit() {
     const params = new HttpParams().set("page", "1").set("pageSize", "10");
