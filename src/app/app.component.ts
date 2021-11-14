70d4df20f7a9e92b9ef1d07c6d50aa578b31e3b8
Jana Semenova <jana.semenova@yolawo.de>
2021-11-14 07:38:04 +0100

    Angular Custom Service - Fetching Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,12 +24,11 @@ export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
   //courses;
 
-  constructor(private http: HttpClient, private coursesService: CoursesService) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
-    const params = new HttpParams().set("page", "1").set("pageSize", "10");
 
-    this.courses$ = this.http.get<Course[]>("/api/courses", { params });
+    this.courses$ = this.coursesService.loadCourses();
 
     // this.http
     //   .get("/api/courses", { params })
