8cfceb58e09677dcbabf6c288814e5275cf44bcf
Jana Semenova <jana.semenova@yolawo.de>
2021-11-14 07:05:34 +0100

    Angular HTTP Client GET calls with Request Param

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,34 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  courses;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+
+    this.http
+      .get("/api/courses", { params })
+      .subscribe((val) => (this.courses = val));
   }
-
-
-
 }
